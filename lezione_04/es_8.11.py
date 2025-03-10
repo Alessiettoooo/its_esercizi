c5c6d5fb28bc586fd152e60a01025ef0f1bca5bf
Alessiettoooo <alessio04092005@gmail.com>
2025-03-10 15:13:59 +0100

    Esercizi_Funzioni

--- lezione_04/es_8.11.py
+++ lezione_04/es_8.11.py
@@ -4,24 +4,28 @@ def show_message(lista) :
     for i in lista :
         print(i)
 
-def send_messages(lista) :
+def send_messages(lista):
     
     sent_messages =[] 
 
-    for i in lista :
-        print(i)
-        sent_messages.append(i)
-        lista.remove(i)
+    while len(lista) > 0:
+        message = lista.pop(0)
+        print(message)
+        sent_messages.append(message)
 
     return sent_messages
 
 messageList =["ciao",  "come ti chiami ? "]
+messageList_new = messageList.copy()
 show_message(messageList) 
 
 sent_messages = send_messages(messageList)
 
 print(messageList)
 print(sent_messages)
+
+print(messageList)
+print(messageList_new)
 '''
 messaggio = "scrivi un messaggio "
 
